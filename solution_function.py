6e2fe8fefea40dc456ba260c82e1c90ca567bce1
VeNi080604 <veni080604@leerling.mosa-RT.be>
2021-05-17 20:31:43 +0200

    versie 2.0

--- solution_function.py
+++ solution_function.py
@@ -13,6 +13,7 @@ def update_score(oldscore, team):
     points_away = oldscore[3]
 
     # schrijf hier de nodige code om de testen te doen slagen
+    set_score = 25
 
     if team == 'H':
         points_home = points_home +1
@@ -20,15 +21,18 @@ def update_score(oldscore, team):
     if team == 'A':
         points_away = points_away +1
 
-    if points_home >= 25:
-        points_home = 0
-        points_away = 0
-        sets_home = sets_home +1
+    if sets_home + sets_away == 4:
+        set_score = 15
 
-    if points_away >= 25:
+    if points_home >= set_score and points_home >= points_away + 2:
         points_home = 0
         points_away = 0
         sets_home = sets_away +1
+
+    if points_away >= set_score and points_away >= points_home + 2:
+        points_home = 0
+        points_away = 0
+        sets_away = sets_away + 1
 
 
     return sets_home, sets_away, points_home, points_away
